9d2f2401c41e0cbf0d71cd765e9e8cac6ed8d0c6
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-01-30 09:53:25 -0600

    Creacion clases
    Getters y setters

--- Torres hanoi/src/Hanoi.java
+++ Torres hanoi/src/Hanoi.java
@@ -1,4 +1,45 @@
 
 public class Hanoi {
+	
+	private int anillo;
+	private int []torre1, torre2, torre3;
+	private int movimientos;
+	
+	public int getAnillo() {
+		return anillo;
+	}
+	public void setAnillo(int anillo) {
+		this.anillo = anillo;
+	}
+	public int [] getTorre1() {
+		return torre1;
+	}
+	public void setTorre1(int [] torre1) {
+		this.torre1 = torre1;
+	}
+	public int [] getTorre2() {
+		return torre2;
+	}
+	public void setTorre2(int [] torre2) {
+		this.torre2 = torre2;
+	}
+	public int [] getTorre3() {
+		return torre3;
+	}
+	public void setTorre3(int [] torre3) {
+		this.torre3 = torre3;
+	}
+	
+	public int getMovimientos() {
+		return movimientos;
+	}
+	public void setMovimientos(int movimientos) {
+		this.movimientos = movimientos;
+	}
+	
+	public int numeroMovimientos() {
+		setMovimientos((int) (Math.pow(2, getAnillo())-1));
+		return movimientos;
+	}
 
 }
--- Torres hanoi/src/Main.java
+++ Torres hanoi/src/Main.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
+private static Scanner sc;
 
+public static void main(String []main) {
+	
+	Hanoi hanoi=new Hanoi();
+	sc = new Scanner(System.in);
+	System.out.println("¿Cuantos anillos tiene tu torre?");
+	hanoi.setAnillo(sc.nextInt() );
+	hanoi.numeroMovimientos();
+	System.out.println("Los movimientos necesarios para esta torre son "+ hanoi.getMovimientos());
+	hanoi.setTorre1(null);
+	hanoi.setTorre2(null);
+	hanoi.setTorre3(null);
+}
 }
