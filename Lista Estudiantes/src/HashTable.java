1bf9bdee6ad31d4378d404b8bf6f8595836d055d
caposcar1998 <42816786+caposcar1998@users.noreply.github.com>
2019-03-23 15:42:48 -0600

    .

--- Lista Estudiantes/src/HashTable.java
+++ Lista Estudiantes/src/HashTable.java
@@ -1,4 +1,18 @@
 
-public class HashTable {
+public class HashTable <K,V>{
+	
+	private Entry <K, Node<V>>[] entries;
+	
+	public HashTable () {
+		entries= new Entry[100];
+	}
+
+	public Entry <K, Node<V>>[] getEntries() {
+		return entries;
+	}
+
+	public void setEntries(Entry <K, Node<V>>[] entries) {
+		this.entries = entries;
+	}
 
 }
