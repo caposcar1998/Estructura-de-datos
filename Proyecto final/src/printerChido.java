77d2c235156b7ed1fa41a46d09263dafe9bf558f
Rabioli <42816870+Rabioli@users.noreply.github.com>
2019-05-06 19:53:02 -0500

    Printer-mejorado

--- Proyecto final/src/printerChido.java
+++ Proyecto final/src/printerChido.java
@@ -1,23 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class printerChido<T extends Comparable<T>> {
 	
 	double distanceY = 65;
 	
+    private int maxLevel(Nodo<T> Nodo) {
+        if (Nodo == null) {
+            return 0;
+        }
+        return Math.max(maxLevel(Nodo.getIzquierda()), maxLevel(Nodo.getDerecha())) + 1;
+    }
+	
     public void printNodo(Nodo<T> nodo, Pane pane) {
         
-        trazarLinea(nodo, pane, pane.getWidth()/2, distanceY, pane.getWidth()/4);
+    	int maxLevel = maxLevel(nodo);
+        trazarLinea(nodo, pane, pane.getWidth()/2, distanceY, pane.getWidth()/5, 1, maxLevel);
         
     }
     
-    private void trazarLinea(Nodo<T>root, Pane pane, double pointX, double pointY, double distanceX) {
+    private void trazarLinea(Nodo<T>root, Pane pane, double pointX, double pointY, double distanceX, int level, int maxLevel) {
+    	
+        int floor = maxLevel - level;
+        distanceX = 7*Math.pow(2, (floor + 1)) - 1;
+        
 		if(root!=null) {
 		root.getCircle().setFill(Color.BLACK);
         root.getCircle().setStroke(Color.BLACK);
@@ -41,7 +48,7 @@ public class printerChido<T extends Comparable<T>> {
 			if(root.getDerecha()!=null) {
 				double difX = pointX+distanceX;
 				double difY = pointY+distanceY;
-				trazarLinea(root.getDerecha(), pane, difX, difY, distanceX/2);
+				trazarLinea(root.getDerecha(), pane, difX, difY, distanceX/2, level+1, maxLevel);
 				root.getLineaDerecha().setStartX(difX);
 			    root.getLineaDerecha().setStartY(difY);
 			    root.getLineaDerecha().setEndX(pointX);
@@ -52,7 +59,7 @@ public class printerChido<T extends Comparable<T>> {
 			if(root.getIzquierda()!=null) {
 				double difX = pointX-distanceX;
 				double difY = pointY+distanceY;
-				trazarLinea(root.getIzquierda(), pane, difX, difY, distanceX/2);
+				trazarLinea(root.getIzquierda(), pane, difX, difY, distanceX/2, level+1, maxLevel);
 				root.getLineaIzquierda().setStartX(difX);
 				root.getLineaIzquierda().setStartY(difY);
 				root.getLineaIzquierda().setEndX(pointX);
